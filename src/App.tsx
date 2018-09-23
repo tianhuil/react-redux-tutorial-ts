410de2f5bee667c699c18302c883a38e0e529ec7
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-09-22 23:20:34 -0400

    remove App.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import * as React  from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Route } from 'react-router-dom'
 
-import './App.css';
 import { FooterComponent } from './components/footer'
 import { AddTodoContainer } from './containers/add-todo'
 import { TodoListContainer } from './containers/todo-list'
@@ -20,27 +19,28 @@ const styles = (theme: Theme) => createStyles({
     margin: theme.spacing.unit,
     maxWidth: 600,
     padding: theme.spacing.unit * 2,
+    textAlign: "center"
   }
 })
 
 const Layout: React.SFC<RouteComponentProps<{filter: string}>> = ({ match: { params } }) => (
-  <div className="App">
-    <Grid container={true} spacing={24} alignItems="center" justify="center">
-      <Grid item={true} xs={12}>
-        <AddTodoContainer />
-        <TodoListContainer filter={params.filter}/>
-        <FooterComponent/>
-      </Grid>
-    </Grid>
+  <div>
+    <AddTodoContainer/>
+    <TodoListContainer filter={params.filter}/>
+    <FooterComponent/>
   </div>
 )
 
 const AppRaw: React.SFC<WithStyles<typeof styles>> = ({classes}) => (
   <Middleware>
     <ThemeComponent>
-      <Paper className={classes.paper}>
-        <Route path="/:filter?" component={Layout}/>
-      </Paper>
+      <Grid container={true} spacing={24} alignItems="center" justify="center">
+        <Grid item={true} xs={12}>
+          <Paper className={classes.paper}>
+            <Route path="/:filter?" component={Layout}/>
+          </Paper>
+        </Grid>
+      </Grid>
     </ThemeComponent>
   </Middleware>
 )
